3bf62b9a571e4e6fc81dcdb85017be03e2202194
joedoe <d_joe@gmx.net>
2024-03-16 13:59:13 +0100

    Refactor and logging

--- py/clean_up_photos.py
+++ py/clean_up_photos.py
@@ -5,42 +5,53 @@ import numpy as np
 import os
 
 if os.uname()[1] == 'raspberrypi':
-    DIR_PHOTOS = '/home/pi/Desktop/traffic_tracker/photos/'
+    ROOT = '/home/pi/Desktop/traffic_tracker'
+    DIR_PHOTOS = f"{ROOT}/photos/"
 else:
-    DIR_PHOTOS = '/home/joe/prog/c/traffic-tracker/v3/traffic_tracker/photos/test/'
-today = datetime.now()
-yesterday = today - timedelta(days=1)
-DIR_PHOTOS += yesterday.strftime("%Y-%m-%d") + '/'
+    ROOT = '/home/joe/prog/c/traffic-tracker/v3/traffic_tracker'
+    DIR_PHOTOS = f"{ROOT}/photos/test/"
+now = datetime.now()
+DIR_PHOTOS += (now - timedelta(days=1)).strftime("%Y-%m-%d")
+FN_LOGS = f"{ROOT}/logs/{now.strftime('%Y-%m-%d')}.log"
+
 MIN_MATCH_VAL = 0.30
+TEST, LOGGING = False, True
 RESIZE = (650, 500)
 WHITE = (255, 255, 255)
-INFO = False
+
+template = cv.imread(f"{ROOT}/py/template.jpg", cv.IMREAD_GRAYSCALE)
+template_width, template_height = template.shape[::-1]
+total, cropped, deleted = 0, 0, 0
 
 
 def match(fn):
-    img = cv.imread(DIR_PHOTOS + fn)
+    global cropped, deleted
+    if TEST:
+        print(fn)
+    img = cv.imread(fn)
     # (h, w) = img.shape[:2]
     # img = img[0: h, 1100:w]
     img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     if np.average(img_gray) < 60:  # SKIP MAINLY BLACK PHOTOS
-        os.remove(DIR_PHOTOS + fn)
-        if INFO:
+        os.remove(fn)
+        deleted += 1
+        if TEST:
             print('Too dark')
         return
-    template = cv.imread('./template.jpg', cv.IMREAD_GRAYSCALE)
-    width_temp, height_temp = template.shape[::-1]
     res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, (x, y) = cv.minMaxLoc(res)
     if max_val > MIN_MATCH_VAL:
-        img_save = img[y: y + height_temp, x: x + width_temp]
-        cv.imwrite(DIR_PHOTOS + fn, img_save)
-        if INFO:
-            print('Match: ' + str(max_val))
+        img_save = img[y: y + template_height, x: x + template_width]
+        cv.imwrite(fn, img_save)
+        cropped += 1
+        if TEST:
+            print(f"Match: {str(max_val)}")
     else:
-        os.remove(DIR_PHOTOS + fn)
-        if INFO:
-            print('No match: ' + str(max_val))
-    if INFO:
+        os.remove(fn)
+        deleted += 1
+        if TEST:
+            print(f"No match: {str(max_val)}")
+    if TEST:
         cv.rectangle(img, (x, y), (x + template.shape[0], y + template.shape[1]), WHITE, 2)
         txt = 'MATCH' if max_val > MIN_MATCH_VAL else 'No match'
         cv.imshow(txt + ' ' + fn + ': ' + str(max_val), cv.resize(img, RESIZE))
@@ -49,13 +60,23 @@ def match(fn):
 
 
 def main():
-    for i, fn in enumerate(os.listdir(DIR_PHOTOS)):
-        if INFO:
-            print(i, ':', fn)
-        match(fn)
+    global total
+    os.chdir(DIR_PHOTOS)
+    if LOGGING:
+        log("INFO", f"Starting script, {os.getcwd()}")
+    for fn in os.listdir():
+        total += 1
+        match(f"{DIR_PHOTOS}/{fn}")
+    if LOGGING:
+        log("INFO", f"Finished script, cropped+deleted/total: {cropped}+{deleted}/{total}")
+
+
+def log(loglevel, msg):
+    entry = f"{datetime.now().strftime('%H:%M:%S')}, {loglevel} [clean_up_photos.py]: {msg}"
+    with open(FN_LOGS, "a+") as f:
+        f.write(entry)
+        f.write("\n")
 
 
 if __name__ == '__main__':
-    if INFO:
-        print("DIR_PHOTOS: " + DIR_PHOTOS)
     main()
